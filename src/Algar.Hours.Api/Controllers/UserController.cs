d1545f4f3881a4f30ce05556f5e6bc644543be1e
erdnando <erdnando@gmail.com>
2024-01-04 18:59:31 -0600

    dynamic response

--- src/Algar.Hours.Api/Controllers/UserController.cs
+++ src/Algar.Hours.Api/Controllers/UserController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using System.Xml.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 
 
 namespace Algar.Hours.Api.Controllers
@@ -90,18 +91,33 @@ AB7XkC7atqVVYhLhRXClgxt45wme
             //Console.WriteLine(samlResponse);
 
             //samlCert = "%3Csamlp%3AResponse%20xmlns%3Asamlp%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Aprotocol%22%20xmlns%3Asaml%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Aassertion%22%20ID%3D%22_8e8dc5f69a98cc4c1ff3427e5ce34606fd672f91e6%22%20Version%3D%222.0%22%20IssueInstant%3D%222014-07-17T01%3A01%3A48Z%22%20Destination%3D%22http%3A%2F%2Fsp.example.com%2Fdemo1%2Findex.php%3Facs%22%20InResponseTo%3D%22ONELOGIN_4fee3b046395c4e751011e97f8900b5273d56685%22%3E%0A%20%20%3Csaml%3AIssuer%3Ehttp%3A%2F%2Fidp.example.com%2Fmetadata.php%3C%2Fsaml%3AIssuer%3E%0A%20%20%3Csamlp%3AStatus%3E%0A%20%20%20%20%3Csamlp%3AStatusCode%20Value%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Astatus%3ASuccess%22%20%2F%3E%0A%20%20%3C%2Fsamlp%3AStatus%3E%0A%20%20%3Csaml%3AAssertion%20xmlns%3Axsi%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema-instance%22%20xmlns%3Axs%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%22%20ID%3D%22_d71a3a8e9fcc45c9e9d248ef7049393fc8f04e5f75%22%20Version%3D%222.0%22%20IssueInstant%3D%222014-07-17T01%3A01%3A48Z%22%3E%0A%20%20%20%20%3Csaml%3AIssuer%3Ehttp%3A%2F%2Fidp.example.com%2Fmetadata.php%3C%2Fsaml%3AIssuer%3E%0A%20%20%20%20%3Csaml%3ASubject%3E%0A%20%20%20%20%20%20%3Csaml%3ANameID%20SPNameQualifier%3D%22http%3A%2F%2Fsp.example.com%2Fdemo1%2Fmetadata.php%22%20Format%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Anameid-format%3Atransient%22%3E_ce3d2948b4cf20146dee0a0b3dd6f69b6cf86f62d7%3C%2Fsaml%3ANameID%3E%0A%20%20%20%20%20%20%3Csaml%3ASubjectConfirmation%20Method%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Acm%3Abearer%22%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3ASubjectConfirmationData%20NotOnOrAfter%3D%222024-01-18T06%3A21%3A48Z%22%20Recipient%3D%22http%3A%2F%2Fsp.example.com%2Fdemo1%2Findex.php%3Facs%22%20InResponseTo%3D%22ONELOGIN_4fee3b046395c4e751011e97f8900b5273d56685%22%20%2F%3E%0A%20%20%20%20%20%20%3C%2Fsaml%3ASubjectConfirmation%3E%0A%20%20%20%20%3C%2Fsaml%3ASubject%3E%0A%20%20%20%20%3Csaml%3AConditions%20NotBefore%3D%222014-07-17T01%3A01%3A18Z%22%20NotOnOrAfter%3D%222024-01-18T06%3A21%3A48Z%22%3E%0A%20%20%20%20%20%20%3Csaml%3AAudienceRestriction%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3AAudience%3Ehttp%3A%2F%2Fsp.example.com%2Fdemo1%2Fmetadata.php%3C%2Fsaml%3AAudience%3E%0A%20%20%20%20%20%20%3C%2Fsaml%3AAudienceRestriction%3E%0A%20%20%20%20%3C%2Fsaml%3AConditions%3E%0A%20%20%20%20%3Csaml%3AAuthnStatement%20AuthnInstant%3D%222014-07-17T01%3A01%3A48Z%22%20SessionNotOnOrAfter%3D%222024-07-17T09%3A01%3A48Z%22%20SessionIndex%3D%22_be9967abd904ddcae3c0eb4189adbe3f71e327cf93%22%3E%0A%20%20%20%20%20%20%3Csaml%3AAuthnContext%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3AAuthnContextClassRef%3Eurn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Aac%3Aclasses%3APassword%3C%2Fsaml%3AAuthnContextClassRef%3E%0A%20%20%20%20%20%20%3C%2Fsaml%3AAuthnContext%3E%0A%20%20%20%20%3C%2Fsaml%3AAuthnStatement%3E%0A%20%20%20%20%3Csaml%3AAttributeStatement%3E%0A%20%20%20%20%20%20%3Csaml%3AAttribute%20Name%3D%22uid%22%20NameFormat%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Aattrname-format%3Abasic%22%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3AAttributeValue%20xsi%3Atype%3D%22xs%3Astring%22%3Etest%3C%2Fsaml%3AAttributeValue%3E%0A%20%20%20%20%20%20%3C%2Fsaml%3AAttribute%3E%0A%20%20%20%20%20%20%3Csaml%3AAttribute%20Name%3D%22mail%22%20NameFormat%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Aattrname-format%3Abasic%22%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3AAttributeValue%20xsi%3Atype%3D%22xs%3Astring%22%3Etest%40example.com%3C%2Fsaml%3AAttributeValue%3E%0A%20%20%20%20%20%20%3C%2Fsaml%3AAttribute%3E%0A%20%20%20%20%20%20%3Csaml%3AAttribute%20Name%3D%22eduPersonAffiliation%22%20NameFormat%3D%22urn%3Aoasis%3Anames%3Atc%3ASAML%3A2.0%3Aattrname-format%3Abasic%22%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3AAttributeValue%20xsi%3Atype%3D%22xs%3Astring%22%3Eusers%3C%2Fsaml%3AAttributeValue%3E%0A%20%20%20%20%20%20%20%20%3Csaml%3AAttributeValue%20xsi%3Atype%3D%22xs%3Astring%22%3Eexamplerole1%3C%2Fsaml%3AAttributeValue%3E%0A%20%20%20%20%20%20%3C%2Fsaml%3AAttribute%3E%0A%20%20%20%20%3C%2Fsaml%3AAttributeStatement%3E%0A%20%20%3C%2Fsaml%3AAssertion%3E%0A%3C%2Fsamlp%3AResponse%3E";
-            string encodedStr = Convert.ToBase64String(Encoding.UTF8.GetBytes("ZXJkbmFuZG9yb2RyaWd1ZXo="));
+           // string encodedStr = Convert.ToBase64String(Encoding.UTF8.GetBytes("ZXJkbmFuZG9yb2RyaWd1ZXo="));
            // var samlResponse = encodedStr;
 
             // 3. DONE!
-            // if (samlResponse.IsValid()) //all good?
-                    {
+             if (samlResponse.IsValid()) //all good?
+             {
                 //WOOHOO!!! the user is logged in
                 // var username = samlResponse.GetNameID(); //let's get the username
                 // var officeLocation = samlResponse.GetCustomAttribute("OfficeAddress");
 
+                var jsonSaml = new {
+                    email = samlResponse.GetEmail(),
+                    nombre = samlResponse.GetFirstName(), 
+                    lastName = samlResponse.GetLastName(), 
+                    location = samlResponse.GetLocation() ,
+                    upn = samlResponse.GetUpn(), 
+                    company = samlResponse.GetCompany(), 
+                    department = samlResponse.GetDepartment(), 
+                    nameid = samlResponse.GetNameID(), 
+                    phone = samlResponse.GetPhone() 
+                    };
 
-               // return Redirect("http://localhost:4200/dashboard?uxm_erd=" + encodedStr);
+               
+                string encodedStr = Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(jsonSaml)));
+
+
+                // return Redirect("http://localhost:4200/dashboard?uxm_erd=" + encodedStr);
                 return Redirect("https://transversal-portaltls-front.shfyjbr2p4o.us-south.codeengine.appdomain.cloud?uxm_erd=" + encodedStr);
 
 
