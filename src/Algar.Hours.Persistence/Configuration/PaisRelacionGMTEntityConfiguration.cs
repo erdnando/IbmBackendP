ea7bf29718c6a3562b0bb6cd86a85a55b820a154
omarin39 <omaralvarez01.oaa@gmail.com>
2024-01-20 00:23:28 -0600

    Add PaisRelacionGMTEntity

---
+++ src/Algar.Hours.Persistence/Configuration/PaisRelacionGMTEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Algar.Hours.Domain.Entities.Country;
+using Algar.Hours.Domain.Entities.PaisRelacionGMT;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algar.Hours.Persistence.Configuration
+{
+    public class PaisRelacionGMTEntityConfiguration
+    {
+        public PaisRelacionGMTEntityConfiguration(EntityTypeBuilder<PaisRelacionGMTEntity> entityBuilder)
+        {
+
+            entityBuilder.HasKey(x => x.IdPaisRelacionGMTEntity);
+            entityBuilder.Property(x => x.NameCountrySelected).IsRequired();
+            entityBuilder.Property(x => x.NameCountryCompare).IsRequired();
+            entityBuilder.Property(x => x.TimeDifference).IsRequired();
+
+        }
+    }
+}
