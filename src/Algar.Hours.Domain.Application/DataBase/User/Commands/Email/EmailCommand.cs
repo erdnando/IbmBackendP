680177fc9f33567e52fd9a8d2e0685701a860ac2
omarin39 <omaralvarez01.oaa@gmail.com>
2024-01-27 21:55:18 -0600

    Email

--- src/Algar.Hours.Domain.Application/DataBase/User/Commands/Email/EmailCommand.cs
+++ src/Algar.Hours.Domain.Application/DataBase/User/Commands/Email/EmailCommand.cs
@@ -65,11 +65,39 @@ namespace Algar.Hours.Application.DataBase.User.Commands.Email
                 case "11": 
                     body="Se han detectado usuarios sin correo registrado en el portal TLS. Favor de solicitar su alta antes de procesarlos";break;
                 case "12":
-                    body = "Su reporte no puede ser procesado, debido a que no se encuentran datos en la pestaña de hora inicio y/o hora fin, por favor comuníquese con su aprobador para obtener más información"; break;
+                    body = "Su reporte no puede ser procesado, debido a que no se encuentran datos en la pestaña de hora inicio y hora fin, por favor comuníquese con su aprobador para obtener mas información"; break;
                 case "13":
-                    body = "Su reporte no puede ser procesado, debido a que usted excedió los límites legales para overtime o stand by, por favor, comuníquese con su coordinador para buscar una excepción gerencial o en su defecto confirme que la información esté correctamente registrada"; break;
+                    body = "El sistema le notifica que usted excedió los límites legales para overtime o stand by, por favor, comuníquese con su coordinador para buscar una excepción gerencial o en su defecto confirme que la información esté correctamente registrada."; break;
                 case "14":
                     body = "Debido a que las horas reportadas se encuentran dentro de su horario laboral su reporte no podrá ser procesado, por favor corrija su reporte y vuelva a intentarlo."; break;
+                case "15":
+                    body = "Su reporte se registró exitosamente y está en proceso de aprobación. Por favor contactar a su aprobador para la gestión de sus reportes, recuerde hacer seguimiento oportuno al ciclo de aprobación y no realizar ediciones en la información."; break;
+                case "16":
+                    body = "Su reporte se registró exitosamente y está en proceso de aprobación. Por favor contactar a su aprobador para la gestión de sus reportes, recuerde hacer seguimiento oportuno al ciclo de aprobación y no realizar ediciones en la información."; break;
+                case "17":
+                    body = "El sistema le notifica que actualmente debe tomar acciones para aprobar o rechazar los tiempos registrados en el portal TLS, Por favor, acceda al portal y verifique sus asignaciones."; break;
+                case "18":
+                    body = "El sistema le notifica que actualmente debe tomar acciones para aprobar o rechazar los tiempos registrados en el portal TLS, Por favor, acceda al portal y verifique sus asignaciones."; break;
+                case "19":
+                    body = "El sistema le notifica que su registro de tiempos de stand by fue aprobado por el nivel 1, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "20":
+                    body = "El sistema le notifica que su registro de tiempos de Overtime fue aprobado por el nivel 1, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "21":
+                    body = "El sistema le notifica que su registro de tiempos de stand by fue aprobado por el nivel 2, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "22":
+                    body = "El sistema le notifica que su registro de tiempos de Overtime fue aprobado por el nivel 2, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "23":
+                    body = "El sistema le notifica que su registro de tiempos de stand by fue rechazado por el nivel 1, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "24":
+                    body = "El sistema le notifica que su registro de tiempos de Overtime fue rechazado por el nivel 1, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "25":
+                    body = "El sistema le notifica que su registro de tiempos de stand by fue rechazado por el nivel 2, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "26":
+                    body = "El sistema le notifica que su registro de tiempos de stand by fue rechazado por el nivel 2, por favor, acceda al portal TLS para dar seguimiento a su proceso de aprobación."; break;
+                case "27":
+                    body = "El sistema le notifica que su horario de trabajo se actualizó si tiene dudas comuníquese con su coordinador."; break;
+                case "28":
+                    body = "El sistema le notifica que su horario de trabajo no se encuentra configurado por favor comuníquese con su coordinador."; break;
                 default:
                     break;
             }
@@ -83,33 +111,61 @@ namespace Algar.Hours.Application.DataBase.User.Commands.Email
             switch (model.Plantilla)
             {
                 case "1":
-                    subject = "ALERTA \"PORTAL TLS\" APROBACION DE HORAS"; break;
+                    subject = "ALERTA \"PORTAL TLS\" APROBACION DE HORAS."; break;
                 case "2":
-                    subject = "ALERTA \"PORTAL TLS \" Su registro fue \"aprobado, rechazado\""; break;
+                    subject = "ALERTA \"PORTAL TLS \" Su registro fue \"aprobado, rechazado\"."; break;
                 case "3":
-                    subject = "ALERTA \"PORTAL TLS \" Su registro fue \"rechazado\""; break;
+                    subject = "ALERTA \"PORTAL TLS \" Su registro fue \"rechazado\"."; break;
                 case "4":
-                    subject = "ALERTA \"PORTAL TLS\" El registro fue rechazado correctamente"; break;
+                    subject = "ALERTA \"PORTAL TLS\" El registro fue rechazado correctamente."; break;
                 case "5":
-                    subject = "ALERTA \"PORTAL TLS \" Su registro fue \"aprobado\""; break;
+                    subject = "ALERTA \"PORTAL TLS \" Su registro fue \"aprobado\"."; break;
                 case "6":
-                    subject = "ALERTA \"PORTAL TLS \" Su horario ha sido modificado"; break;
+                    subject = "ALERTA \"PORTAL TLS \" Su horario ha sido modificado."; break;
                 case "7":
-                    subject = "ALERTA \"PORTAL TLS \" Sus límites legales han sido modificados"; break;
+                    subject = "ALERTA \"PORTAL TLS \" Sus límites legales han sido modificados."; break;
                 case "8":
-                    subject = "ALERTA \"PORTAL TLS \" NO APLICA POR HORARIO"; break;
+                    subject = "ALERTA \"PORTAL TLS \"Usuario sin horarios."; break;
                 case "9":
-                    subject = "ALERTA \"PORTAL TLS \" NO APLICA POR OVERTIME"; break;
+                    subject = "ALERTA \"PORTAL TLS \" NO APLICA POR OVERTIME."; break;
                 case "10":
-                    subject = "ALERTA \"PORTAL TLS \" NO APLICA POR OVERLAPING"; break;
+                    subject = "ALERTA \"PORTAL TLS \" NO APLICA POR OVERLAPING."; break;
                 case "11":
-                    subject = "ALERTA \"PORTAL TLS \" USUARIOS SIN EMAIL EN SISTEMA TLS DURANTE EL PROCESO DE CARGA (ARP, TSE O STE"; break;
+                    subject = "ALERTA \"PORTAL TLS \" USUARIOS SIN EMAIL EN SISTEMA TLS DURANTE EL PROCESO DE CARGA (ARP, TSE O STE)."; break;
                 case "12":
-                    subject = "ALERTA \"PORTAL TLS \" NO APLICA SU REPORTE POR FALTA DE DATOS (HORA INICIO/ HORA FIN)"; break;
+                    subject = "ALERTA \"PORTAL TLS \" No aplica su reporte por falta de datos."; break;
                 case "13":
-                    subject = "ALERTA \"PORTAL TLS \"NO APLICA SU REPORTE POR EXCESO DE LÍMITES LEGALES DE TRABAJO"; break;
+                    subject = "ALERTA \"PORTAL TLS \"límites de overtime o stand by excedido."; break;
                 case "14":
-                    subject = "ALERTA \"PORTAL TLS \"NO APLICA SU REPORTE POR QUE SUS HORAS REPORTADAS SE ENCUENTRAN DENTRO DEL HORARIO LABORAL"; break;
+                    subject = "ALERTA \"PORTAL TLS \"Sus horas reportadas se encuentran dentro de su horario laboral."; break;
+                case "15":
+                    subject = "ALERTA \"PORTAL TLS \"Registro exitoso de Stand by."; break;
+                case "16":
+                    subject = "ALERTA \"PORTAL TLS \"Registro exitoso de Overtime."; break;
+                case "17":
+                    subject = "ALERTA \"PORTAL TLS \"Acción necesaria - aprobación Stand by."; break;
+                case "18":
+                    subject = "ALERTA \"PORTAL TLS \"Acción necesaria - aprobación Overtime."; break;
+                case "19":
+                    subject = "ALERTA \"PORTAL TLS \"Aprobación exitosa de Stand by."; break;
+                case "20":
+                    subject = "ALERTA \"PORTAL TLS \"Aprobación exitosa de Overtime."; break;
+                case "21":
+                    subject = "ALERTA \"PORTAL TLS \"Aprobación exitosa de Stand by."; break;
+                case "22":
+                    subject = "ALERTA \"PORTAL TLS \"Aprobación exitosa de Overtime."; break;
+                case "23":
+                    subject = "ALERTA \"PORTAL TLS \"El registro fue rechazado correctamente."; break;
+                case "24":
+                    subject = "ALERTA \"PORTAL TLS \"El registro fue rechazado correctamente."; break;
+                case "25":
+                    subject = "ALERTA \"PORTAL TLS \"El registro fue rechazado correctamente."; break;
+                case "26":
+                    subject = "ALERTA \"PORTAL TLS \"El registro fue rechazado correctamente."; break;
+                case "27":
+                    subject = "ALERTA \"PORTAL TLS \"Actualización de horario de trabajo."; break;
+                case "28":
+                    subject = "ALERTA \"PORTAL TLS \"actualización de horario de trabajo."; break;
                 default:
                     break;
             }
