2938a4c8b69a0aa6bd86855749727713af587aa6
Erdnando <erdnandovr@algartech.com>
2024-02-26 16:36:27 -0600

    bug fixed , daemon 10

--- src/Algar.Hours.Domain.Application/DataBase/User/Commands/Login/LoginUserCommand.cs
+++ src/Algar.Hours.Domain.Application/DataBase/User/Commands/Login/LoginUserCommand.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
@@ -37,11 +38,39 @@ namespace Algar.Hours.Application.DataBase.User.Commands.Login
                 .Include(e => e.CountryEntity)
                 .Include(e => e.RoleEntity)
                 .Where(x => x.Email.ToLower().Trim() == model.UserName.ToLower().Trim() && x.Password == model.Password).FirstOrDefault();
+
+
             var ModelUser = _mapper.Map<CreateUserModel>(entity);
+            await daemon10();
+
+            if (entity != null)
+            {
+                await _logCommand.Log(entity.IdUser.ToString(), "Log In al sistema", model);
+            }
+            else
+            {
+                await _logCommand.Log(model.UserName.ToString(), "Intento fallido de Log In al sistema", model);
+            }
+
+
+            return ModelUser;
+
+        }
+
+        private async Task daemon10()
+        {
+            try
+            {
+
+            
             var RowGralFS = _databaseService.HorusReportEntity.Where(op => (op.EstatusOrigen == "FINAL" || op.EstatusOrigen == "SUBMITTED") && op.EstatusFinal != "DESCARTADO").ToList();
-            //var RowGralFS10 = RowGralFS.Where(op =>(DateTime.Now - DateTime.Parse(op.strCreationDate)).Days > 10).ToList();
-           // var RowGralFS10 = RowGralFS.Where(op => (DateTime.Now - DateTime.Parse(op.strCreationDate.Substring(0, 10))).Days > 10).ToList();
-           /*
+            var RowGralFS10 = RowGralFS.Where(op => (
+                                DateTime.ParseExact(DateTime.Now.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture), "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+                                -
+                                DateTime.ParseExact(op.strCreationDate.Substring(0, 10), "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)).Days > 10
+                                && op.EstatusFinal != "DESCARTADO"
+                                ).ToList();
+
 
             List<Domain.Entities.AssignmentReport.AssignmentReport> rowAssignments = new();
             foreach (var item10 in RowGralFS10)
@@ -64,22 +93,12 @@ namespace Algar.Hours.Application.DataBase.User.Commands.Login
                 await _logCommand.Log("53765c41-411f-4add-9034-7debaf04f276", "DESCARTA REGISTRO", item10);
             }
             _databaseService.assignmentReports.AddRangeAsync(rowAssignments);
-            await _databaseService.SaveAsync();*/
-
-            if (entity != null)
+            await _databaseService.SaveAsync();
+            }catch(Exception ex)
             {
-                await _logCommand.Log(entity.IdUser.ToString(), "Log In al sistema", model);
-            }
-            else
-            {
-                await _logCommand.Log(model.UserName.ToString(), "Intento fallido de Log In al sistema", model);
-            }
-           
 
-            return ModelUser;
-
+            }
         }
 
-       
     }
 }
