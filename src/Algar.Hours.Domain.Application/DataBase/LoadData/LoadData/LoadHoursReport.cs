08809f7a6e1d679a1cd8f3d88e7b16f85ab02e9a
Omar Alvarez Alcántara <omaralvarez01.oaa@gmail.com>
2024-03-15 19:15:59 -0600

    Update Horas
    Update Horas

--- src/Algar.Hours.Domain.Application/DataBase/LoadData/LoadData/LoadHoursReport.cs
+++ src/Algar.Hours.Domain.Application/DataBase/LoadData/LoadData/LoadHoursReport.cs
@@ -411,7 +411,17 @@ namespace Algar.Hours.Application.DataBase.LoadData.LoadData
                     var previosAndPos = new List<double>();
                     if (horario.Count >0)
                     {
-                        previosAndPos = getPreviasAndPosHorario(arp.HORA_INICIO, arp.HORA_FIN, horario[indexHorario].HoraInicio, horario[indexHorario].HoraFin);
+                        if (indexHorario>=0)
+                        {
+                            previosAndPos = getPreviasAndPosHorario(arp.HORA_INICIO, arp.HORA_FIN, horario[indexHorario].HoraInicio, horario[indexHorario].HoraFin);
+                        }
+                        else
+                        {
+                            parametersARP.EstatusProceso = "NO_APLICA_X_HORARIO";
+                            listParametersInitialEntity.Add(parametersARP);
+                            continue;
+                        }
+                        
                     }
                     else
                     {
